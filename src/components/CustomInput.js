0ee70fdaa950101bcdda2d68e0f206f1958ee326
Luom Pham <luompham95@gmail.com>
2023-03-24 06:32:34 +0700

    update

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const CustomInput = (props) => {
-  const { type, label, i_id, i_class } = props;
+  const { type, label, i_id, i_class, name, val, onChange, onBlur } = props;
   return (
     <>
       <div className="form-floating mb-3">
@@ -10,6 +10,10 @@ const CustomInput = (props) => {
           className={`form-control ${i_class}`}
           id={i_id}
           placeholder={label}
+          name={name}
+          value={val}
+          onChange={onChange}
+          onBlur={onChange}
         />
         <label htmlFor={label}>{label}</label>
       </div>
